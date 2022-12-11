dd936e417553153d1b270ff25660a1ad7880fb80
ademaaitu <ademaamangeldi111@gmail.com>
2022-12-11 20:06:20 +0600

    rolling

---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,69 @@
+
+package com.example.diceroller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Это действие позволяет пользователю бросить кости и просмотреть результат.
+ * на экране.
+ */
+class MainActivity : AppCompatActivity() {
+
+    /**
+    *Этот метод вызывается при создании Activity.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Find the Button in the layout
+        val rollButton: Button = findViewById(R.id.button)
+
+        // Set a click listener on the button to roll the dice when the user taps the button
+        rollButton.setOnClickListener { rollDice() }
+    }
+
+    /**
+     *Бросьте кости и обновите экран с результатом.
+     */
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+        val diceImage: ImageView = findViewById(R.id.imageView)
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
+
+        diceImage.contentDescription = diceRoll.toString()
+    }
+}
+fun main() {
+    val myFirstDice = Dice(6)
+    val rollResult = myFirstDice.roll()
+    val luckyNumber = 4
+
+    when (rollResult) {
+        luckyNumber -> println("You won!")
+        1 -> println("So sorry! You rolled a 1. Try again!")
+        2 -> println("Sadly, you rolled a 2. Try again!")
+        3 -> println("Unfortunately, you rolled a 3. Try again!")
+        5 -> println("Don't cry! You rolled a 5. Try again!")
+        6 -> println("Apologies! You rolled a 6. Try again!")
+    }
+}
+
+class Dice(val numSides: Int) {
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
